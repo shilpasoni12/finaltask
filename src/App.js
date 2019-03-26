3ff595751c3f9b9e23e6d9cbf44f69fc29976b55
shilpasoni12 <48425809+shilpasoni12@users.noreply.github.com>
2019-03-26 18:31:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-// import './App.css';
+import './login.css';
 import './mm.jpg';
 import {BrowserRouter as Router, Link} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 
 class Login extends Component {
@@ -24,15 +27,17 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="main">
-        <div id="login">
-          <p>LOG IN</p>
+          <div>
+        <div className="main">
+        <div id="login"  className="login">
+          <p className="heading"><h2>LOGIN</h2></p>
           <input type="email" id="mail" ref="email" placeholder="Enter Email ID"/>
           <br/>
           <input type="password" id="pwd" ref="pwd" placeholder="Enter Password"/>
           <br/>
           <button onClick={this.btnclick}>Login</button>
         </div>
+      </div>
       </div>
         );
     }
